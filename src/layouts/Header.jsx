d494533cee3cd5436ad0f6de370fbc9abc1a0fa2
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-01 20:03:05 -0300

    Refactor Header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,55 +1,85 @@
-import { Button, IconButton, useMediaQuery, useTheme } from '@mui/material'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import { Menu } from '@mui/icons-material'
+// import { Button, IconButton, useMediaQuery, useTheme } from '@mui/material'
+// import AppBar from '@mui/material/AppBar'
+// import Box from '@mui/material/Box'
+// import Toolbar from '@mui/material/Toolbar'
+// import Typography from '@mui/material/Typography'
+// import { Menu as MenuMUI } from '@mui/icons-material'
 import { useTaskView } from '../hooks/useTaskView'
 import { VIEW_TYPES } from '../constants/viewTypes'
 import CreateTaskListDialog from '../components/CreateTaskListDialog'
+import { Menu } from 'lucide-react'
 
 export default function Header({ setOpenSidebar }) {
   const { getCurrentViewName, currentView } = useTaskView()
 
-  const theme = useTheme()
-  const isDesktop = useMediaQuery(theme.breakpoints.up('sm'))
-
   return (
-    <Box>
-      <AppBar position='static'>
-        <Toolbar
-          sx={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(3,1fr)',
-            justifyItems: 'center',
-          }}
-        >
-          {!isDesktop && (
-            <Box justifySelf={'left'}>
-              <IconButton
-                edge='start'
+    <div>
+      <div>
+        <header>
+          <div className='grid grid-cols-3 border-b-1 border-b-gray-300 p-4'>
+            <div className='flex items-center'>
+              <button
+                className='p-1'
                 onClick={() => setOpenSidebar(true)}
               >
                 <Menu />
-              </IconButton>
-            </Box>
-          )}
-          <Typography
-            variant='h1'
-            fontSize={'1.4rem'}
+              </button>
+            </div>
+            <div className='flex items-center justify-center'>
+              <span className='text-xl'>{getCurrentViewName()}</span>
+            </div>
+            <div className='flex items-center justify-end'>
+              <button
+                className='p-1'
+                onClick={() => setOpenSidebar(true)}
+              >
+                {currentView.type === VIEW_TYPES.LIST && (
+                  <CreateTaskListDialog
+                    mode='edit'
+                    listId={currentView.listId}
+                  />
+                )}
+              </button>
+            </div>
+          </div>
+        </header>
+      </div>
+      {/* <Box>
+        <AppBar position='static'>
+          <Toolbar
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(3,1fr)',
+              justifyItems: 'center',
+            }}
           >
-            {getCurrentViewName()}
-          </Typography>
-          <Box justifySelf={'end'}>
-            {currentView.type === VIEW_TYPES.LIST && (
-              <CreateTaskListDialog
-                mode='edit'
-                listId={currentView.listId}
-              />
+            {!isDesktop && (
+              <Box justifySelf={'left'}>
+                <IconButton
+                  edge='start'
+                  onClick={() => setOpenSidebar(true)}
+                >
+                  <MenuMUI />
+                </IconButton>
+              </Box>
             )}
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+            <Typography
+              variant='h1'
+              fontSize={'1.4rem'}
+            >
+              {getCurrentViewName()}
+            </Typography>
+            <Box justifySelf={'end'}>
+              {currentView.type === VIEW_TYPES.LIST && (
+                <CreateTaskListDialog
+                  mode='edit'
+                  listId={currentView.listId}
+                />
+              )}
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>*/}
+    </div>
   )
 }
