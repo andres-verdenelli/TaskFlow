285dd01f6abd6a1bf872d343883580a4e98a595e
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-04 08:47:12 -0300

    Migrate TaskLisk to tailwind

--- src/components/TaskListsSection.jsx
+++ src/components/TaskListsSection.jsx
@@ -1,14 +1,43 @@
-import TaskList from './TaskList'
 import { VIEW_TYPES } from '../constants/viewTypes'
 import { useTaskView } from '../hooks/useTaskView'
 import { useLists } from '../hooks/useLists'
+import { Circle } from 'lucide-react'
+// import ListItemButton from '@mui/material/ListItemButton'
+// import TaskList from './TaskList'
+// import ListItemText from '@mui/material/ListItemText'
+// import ListItemIcon from '@mui/material/ListItemIcon'
+// import CircleIcon from '@mui/icons-material/Circle'
 
 export default function TaskListSection({ setOpenSidebar }) {
   const { lists } = useLists()
   const { currentView, setCurrentView } = useTaskView()
+
+  const TaskListFn = list => {
+    const isSelected =
+      currentView.type === VIEW_TYPES.LIST && currentView.listId === list.id
+    return (
+      <>
+        <button
+          className={`flex w-full cursor-pointer p-4 hover:bg-gray-100 ${isSelected && 'bg-gray-100'}`}
+          onClick={() => {
+            setCurrentView({ type: VIEW_TYPES.LIST, listId: list.id })
+            setOpenSidebar(false)
+          }}
+          key={list.id}
+        >
+          <div className='mr-2'>
+            <Circle />
+          </div>
+          <div>
+            <span>{list.name}</span>
+          </div>
+        </button>
+      </>
+    )
+  }
   return (
     <>
-      {lists.map(list => (
+      {/* {lists.map(list => (
         <TaskList
           key={list.id}
           name={list.name}
@@ -22,7 +51,8 @@ export default function TaskListSection({ setOpenSidebar }) {
           }}
           color={list.color}
         />
-      ))}
+      ))} */}
+      {lists.map(list => TaskListFn(list))}
     </>
   )
 }
