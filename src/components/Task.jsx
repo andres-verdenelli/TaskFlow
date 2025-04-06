82b4eb361fed3b2a612f6e6e09933fa535392582
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-06 18:51:39 -0300

    Migrate Task.jsx

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,31 +1,28 @@
-import ListItem from '@mui/material/ListItem'
-import Checkbox from '@mui/material/Checkbox'
-import TextField from '@mui/material/TextField'
-import DeleteIcon from '@mui/icons-material/Delete'
-import IconButton from '@mui/material/IconButton'
 import { useTasks } from '../hooks/useTasks'
-import EditIcon from '@mui/icons-material/Edit'
+import { Trash2, PenLine } from 'lucide-react'
 
 export default function Task({ task }) {
   const { deleteTask, renameTask, checkTask } = useTasks()
   return (
-    <ListItem sx={{ display: 'flex', alignItems: 'center' }}>
-      <Checkbox
-        checked={task.isDone}
-        onChange={() => checkTask(task.id)}
-      />
-      <TextField
-        sx={{ flexGrow: 1, mr: 2 }}
-        variant='standard'
-        value={task.name}
-        onChange={e => renameTask(task.id, e.target.value)}
-      ></TextField>
-      <IconButton onClick={() => deleteTask(task.id)}>
-        <DeleteIcon />
-      </IconButton>
-      <IconButton>
-        <EditIcon />
-      </IconButton>
-    </ListItem>
+    <>
+      <div className='flex'>
+        <input
+          type='checkbox'
+          checked={task.isDone}
+          onChange={() => checkTask(task.id)}
+        />
+        <input
+          type='text'
+          onChange={e => renameTask(task.id, e.target.value)}
+          value={task.name}
+        />
+        <div onClick={() => deleteTask(task.id)}>
+          <Trash2 />
+        </div>
+        <div>
+          <PenLine />
+        </div>
+      </div>
+    </>
   )
 }
