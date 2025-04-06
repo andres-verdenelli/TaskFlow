389e87406179c716fd64e2280fc404d68ef6e70b
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-06 18:43:01 -0300

    Remove comented code in Header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,9 +1,3 @@
-// import { Button, IconButton, useMediaQuery, useTheme } from '@mui/material'
-// import AppBar from '@mui/material/AppBar'
-// import Box from '@mui/material/Box'
-// import Toolbar from '@mui/material/Toolbar'
-// import Typography from '@mui/material/Typography'
-// import { Menu as MenuMUI } from '@mui/icons-material'
 import { useTaskView } from '../hooks/useTaskView'
 import { VIEW_TYPES } from '../constants/viewTypes'
 import CreateTaskListDialog from '../components/CreateTaskListDialog'
@@ -44,42 +38,6 @@ export default function Header({ setOpenSidebar }) {
           </div>
         </header>
       </div>
-      {/* <Box>
-        <AppBar position='static'>
-          <Toolbar
-            sx={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(3,1fr)',
-              justifyItems: 'center',
-            }}
-          >
-            {!isDesktop && (
-              <Box justifySelf={'left'}>
-                <IconButton
-                  edge='start'
-                  onClick={() => setOpenSidebar(true)}
-                >
-                  <MenuMUI />
-                </IconButton>
-              </Box>
-            )}
-            <Typography
-              variant='h1'
-              fontSize={'1.4rem'}
-            >
-              {getCurrentViewName()}
-            </Typography>
-            <Box justifySelf={'end'}>
-              {currentView.type === VIEW_TYPES.LIST && (
-                <CreateTaskListDialog
-                  mode='edit'
-                  listId={currentView.listId}
-                />
-              )}
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </Box>*/}
     </div>
   )
 }
