cbc8462e06c507188fedab26fcab8f04de5ab2a5
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-06 18:45:24 -0300

    Remove commented code in NavigationItems.jsx

--- src/components/NavigationItems.jsx
+++ src/components/NavigationItems.jsx
@@ -1,6 +1,3 @@
-// import { GridView } from '@mui/icons-material'
-// import { ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
-// import { Today, Schedule, TaskAlt } from '@mui/icons-material'
 import { VIEW_TYPES } from '../constants/viewTypes'
 import { useTaskView } from '../hooks/useTaskView'
 import { LayoutDashboard, Calendar1, Clock, CircleCheckBig } from 'lucide-react'
@@ -35,55 +32,6 @@ export default function NavigationItems({ setOpenSidebar }) {
         {ListItem('Scheduled', Clock, VIEW_TYPES.SCHEDULE)}
         {ListItem('Done', CircleCheckBig, VIEW_TYPES.DONE)}
       </div>
-      {/* 
-      <ListItemButton
-        selected={currentView.type === VIEW_TYPES.ALL}
-        onClick={() => {
-          setCurrentView({ type: VIEW_TYPES.ALL })
-          setOpenSidebar(false)
-        }}
-      >
-        <ListItemIcon>
-          <GridView />
-        </ListItemIcon>
-        <ListItemText>All</ListItemText>
-      </ListItemButton>
-      <ListItemButton
-        selected={currentView.type === VIEW_TYPES.TODAY}
-        onClick={() => {
-          setCurrentView({ type: VIEW_TYPES.TODAY })
-          setOpenSidebar(false)
-        }}
-      >
-        <ListItemIcon>
-          <Today />
-        </ListItemIcon>
-        <ListItemText>Today</ListItemText>
-      </ListItemButton>
-      <ListItemButton
-        selected={currentView.type === VIEW_TYPES.SCHEDULE}
-        onClick={() => {
-          setCurrentView({ type: VIEW_TYPES.SCHEDULE })
-          setOpenSidebar(false)
-        }}
-      >
-        <ListItemIcon>
-          <Schedule />
-        </ListItemIcon>
-        <ListItemText>Scheduled</ListItemText>
-      </ListItemButton>
-      <ListItemButton
-        selected={currentView.type === VIEW_TYPES.DONE}
-        onClick={() => {
-          setCurrentView({ type: VIEW_TYPES.DONE })
-          setOpenSidebar(false)
-        }}
-      >
-        <ListItemIcon>
-          <TaskAlt />
-        </ListItemIcon>
-        <ListItemText>Done</ListItemText>
-      </ListItemButton> */}
     </>
   )
 }
