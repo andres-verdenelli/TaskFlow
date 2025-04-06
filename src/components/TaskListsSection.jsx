2be7b26da81228a631bab7a75f48235f93f064eb
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-06 18:25:52 -0300

    Add key for mapped components

--- src/components/TaskListsSection.jsx
+++ src/components/TaskListsSection.jsx
@@ -12,27 +12,24 @@ export default function TaskListSection({ setOpenSidebar }) {
   const { lists } = useLists()
   const { currentView, setCurrentView } = useTaskView()
 
-  const TaskListFn = list => {
+  const TaskListFn = ({ list }) => {
     const isSelected =
       currentView.type === VIEW_TYPES.LIST && currentView.listId === list.id
     return (
-      <>
-        <button
-          className={`flex w-full cursor-pointer p-4 hover:bg-gray-100 ${isSelected && 'bg-gray-100'}`}
-          onClick={() => {
-            setCurrentView({ type: VIEW_TYPES.LIST, listId: list.id })
-            setOpenSidebar(false)
-          }}
-          key={list.id}
-        >
-          <div className='mr-2'>
-            <Circle />
-          </div>
-          <div>
-            <span>{list.name}</span>
-          </div>
-        </button>
-      </>
+      <button
+        className={`flex w-full cursor-pointer p-4 hover:bg-gray-100 ${isSelected && 'bg-gray-100'}`}
+        onClick={() => {
+          setCurrentView({ type: VIEW_TYPES.LIST, listId: list.id })
+          setOpenSidebar(false)
+        }}
+      >
+        <div className='mr-2'>
+          <Circle />
+        </div>
+        <div>
+          <span>{list.name}</span>
+        </div>
+      </button>
     )
   }
   return (
@@ -52,7 +49,12 @@ export default function TaskListSection({ setOpenSidebar }) {
           color={list.color}
         />
       ))} */}
-      {lists.map(list => TaskListFn(list))}
+      {lists.map(list => (
+        <TaskListFn
+          key={list.id}
+          list={list}
+        />
+      ))}
     </>
   )
 }
