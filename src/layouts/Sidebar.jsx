c41a92a67b187a8d4bc1f697fa6ae81d51f3843e
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-08 16:29:37 -0300

    Remove MUI from Sidebar.jsx

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,32 +1,32 @@
-import { Drawer } from '@mui/material'
 import NavigationItems from '../components/NavigationItems'
 import TaskListSection from '../components/TaskListsSection'
 import CreateListDialog from '../components/CreateTaskListDialog'
 
 export default function Sidebar({ openSidebar, setOpenSidebar }) {
-  const SidebarContent = () => (
-    <div className='flex h-full min-w-50 flex-col justify-between border-r-1 border-gray-200'>
-      <ul>
-        <NavigationItems setOpenSidebar={setOpenSidebar} />
-        <div className='w-full border-b-1 border-gray-300'></div>
-        <TaskListSection setOpenSidebar={setOpenSidebar} />
-      </ul>
-
-      <div
-        onClick={e => e.stopPropagation()}
-        className='flex content-center justify-center p-1'
-      >
-        <CreateListDialog />
-      </div>
-    </div>
-  )
-
   return (
-    <Drawer
-      open={openSidebar}
-      onClose={() => setOpenSidebar(false)}
-    >
-      <SidebarContent />
-    </Drawer>
+    <>
+      {openSidebar && (
+        <div className='fixed inset-0'>
+          <div
+            className='fixed inset-0 z-50 bg-black opacity-15'
+            onClick={() => setOpenSidebar(false)}
+          ></div>
+          <div className='fixed z-51 flex h-full min-w-50 flex-col justify-between border-r-1 border-gray-200 bg-white'>
+            <ul>
+              <NavigationItems setOpenSidebar={setOpenSidebar} />
+              <div className='w-full border-b-1 border-gray-300'></div>
+              <TaskListSection setOpenSidebar={setOpenSidebar} />
+            </ul>
+
+            <div
+              onClick={e => e.stopPropagation()}
+              className='flex content-center justify-center p-1'
+            >
+              <CreateListDialog />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
