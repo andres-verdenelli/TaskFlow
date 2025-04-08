59ee0412c1d25f42890d4abe64caba8e7be6257f
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-08 16:10:23 -0300

    Remove MUI from AppProvider.jsx

--- src/providers/AppProvider.jsx
+++ src/providers/AppProvider.jsx
@@ -1,22 +1,15 @@
 import { ListProvider } from '../context/ListContext'
 import { TaskProvider } from '../context/TaskContext'
-import { ThemeContext } from '../context/ThemeContext'
 import { ViewProvider } from '../context/ViewContext'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 export function AppProvider({ children }) {
   return (
     <>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <ThemeContext>
-          <ListProvider>
-            <TaskProvider>
-              <ViewProvider>{children}</ViewProvider>
-            </TaskProvider>
-          </ListProvider>
-        </ThemeContext>
-      </LocalizationProvider>
+      <ListProvider>
+        <TaskProvider>
+          <ViewProvider>{children}</ViewProvider>
+        </TaskProvider>
+      </ListProvider>
     </>
   )
 }
