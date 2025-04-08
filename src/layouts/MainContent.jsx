e245652ae11662d64f0a8af3034d11db649d2648
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-08 16:08:29 -0300

    Migrate MainContent.jsx to tailwind

--- src/layouts/MainContent.jsx
+++ src/layouts/MainContent.jsx
@@ -1,22 +1,21 @@
-import { Box, List } from '@mui/material'
 import Task from '../components/Task'
 import { CreateTaskDialog } from '../components/CreateTaskDialog'
 import { useTaskView } from '../hooks/useTaskView'
-import { VIEW_TYPES } from '../constants/viewTypes'
 
 export default function MainContent() {
-  const { visibleTasks, currentView } = useTaskView()
+  const { visibleTasks } = useTaskView()
+
   return (
-    <Box flexGrow={1}>
-      <List>
+    <div className='grow'>
+      <li>
         {visibleTasks.map(task => (
           <Task
             task={task}
             key={task.id}
           />
         ))}
-      </List>
+      </li>
       <CreateTaskDialog />
-    </Box>
+    </div>
   )
 }
