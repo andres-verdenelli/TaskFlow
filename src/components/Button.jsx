263c69c699e1028ea6383439bab75cfddc3ffdfe
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-08 18:26:47 -0300

    Create Button.jsx component

---
+++ src/components/Button.jsx
@@ -0,0 +1,25 @@
+export default function Button({
+  children,
+  variant = 'primary',
+  className = '',
+  ...props
+}) {
+  const baseClasses = 'rounded-lg border-1 px-4 py-2'
+  const variantClasses = {
+    primary: 'bg-neutral-800 text-white',
+    secondary: 'bg-zinc-100  border-zinc-200',
+    destructive: 'bg-red-700 border-red-700 text-white',
+    outline: 'bg-white border-zinc-300',
+  }
+
+  const buttonClasses = `${baseClasses} ${variantClasses[variant]} ${className}`
+
+  return (
+    <button
+      className={buttonClasses}
+      {...props}
+    >
+      <div className='flex items-center justify-center'>{children}</div>
+    </button>
+  )
+}
