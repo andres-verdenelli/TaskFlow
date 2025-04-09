fc521691ec0ca12eadcd95fa036df7211e145f86
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-09 15:32:01 -0300

    Remove unnecesari <></> from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,17 @@ import { useState } from 'react'
 
 export default function App() {
   const [openSidebar, setOpenSidebar] = useState(false)
+
   return (
-    <>
-      <>
-        <div className='flex h-screen flex-col'>
-          <Header setOpenSidebar={setOpenSidebar} />
-          <div className='flex grow'>
-            <Sidebar
-              openSidebar={openSidebar}
-              setOpenSidebar={setOpenSidebar}
-            />
-            <MainContent />
-          </div>
-        </div>
-      </>
-    </>
+    <div className='flex h-screen flex-col'>
+      <Header setOpenSidebar={setOpenSidebar} />
+      <div className='flex grow'>
+        <Sidebar
+          openSidebar={openSidebar}
+          setOpenSidebar={setOpenSidebar}
+        />
+        <MainContent />
+      </div>
+    </div>
   )
 }
