f3fe08174f4bd2fe17bccb596bd02baa177fceec
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-09 18:36:34 -0300

    Refactor Sidebar.jsx to improve code structure and reusability of list item components

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -14,60 +14,55 @@ export default function Sidebar({ setSidebarOpen }) {
   const { lists } = useLists()
   const { currentView, setCurrentView } = useTaskView()
 
-  const TaskListFn = ({ list }) => {
-    const isSelected =
-      currentView.type === VIEW_TYPES.LIST && currentView.listId === list.id
+  const ListItemButton = (text, Icon, isSelected, onClick) => {
     return (
-      <button
-        className={`flex w-full cursor-pointer p-4 hover:bg-gray-100 ${isSelected && 'bg-gray-100'}`}
-        onClick={() => {
-          setCurrentView({ type: VIEW_TYPES.LIST, listId: list.id })
-          setSidebarOpen(false)
-        }}
-      >
-        <div className='mr-2'>
-          <CircleSmall />
-        </div>
-        <div>
-          <span>{list.name}</span>
-        </div>
-      </button>
+      <li>
+        <button
+          className={`flex w-full cursor-pointer rounded-md p-2 hover:bg-gray-100 ${isSelected && 'bg-gray-100'}`}
+          onClick={onClick}
+        >
+          <div className='mr-2'>
+            <Icon />
+          </div>
+          <div>
+            <span>{text}</span>
+          </div>
+        </button>
+      </li>
     )
   }
 
   const ListItem = (text, Icon, viewType) => {
     const isSelected = currentView.type === viewType
+    const onClick = () => {
+      setCurrentView({ type: viewType })
+      setSidebarOpen(false)
+    }
 
-    return (
-      <button
-        className={`mb-2 flex w-full cursor-pointer rounded-md p-2 hover:bg-gray-100 ${isSelected && 'bg-gray-100'}`}
-        onClick={() => {
-          setCurrentView({ type: viewType })
-          setSidebarOpen(false)
-        }}
-      >
-        <div className='mr-2'>
-          <Icon />
-        </div>
-        <div>
-          <span>{text}</span>
-        </div>
-      </button>
-    )
+    return ListItemButton(text, Icon, isSelected, onClick)
+  }
+
+  const TaskList = ({ list }) => {
+    const isSelected =
+      currentView.type === VIEW_TYPES.LIST && currentView.listId === list.id
+
+    const onClick = () => {
+      setCurrentView({ type: VIEW_TYPES.LIST, listId: list.id })
+      setSidebarOpen(false)
+    }
+    return ListItemButton(list.name, CircleSmall, isSelected, onClick)
   }
 
   return (
-    <div className='flex h-full min-w-50 flex-col justify-between border-r-1 border-gray-200 bg-white'>
-      <ul>
-        <div className='p-2'>
-          {ListItem('All', LayoutDashboard, VIEW_TYPES.ALL)}
-          {ListItem('Today', Calendar1, VIEW_TYPES.TODAY)}
-          {ListItem('Scheduled', Clock, VIEW_TYPES.SCHEDULE)}
-          {ListItem('Done', CircleCheckBig, VIEW_TYPES.DONE)}
-        </div>
+    <div className='flex h-full min-w-50 flex-col justify-between border-r-1 border-gray-200 bg-white p-4'>
+      <ul className='flex flex-col gap-2'>
+        {ListItem('All', LayoutDashboard, VIEW_TYPES.ALL)}
+        {ListItem('Today', Calendar1, VIEW_TYPES.TODAY)}
+        {ListItem('Scheduled', Clock, VIEW_TYPES.SCHEDULE)}
+        {ListItem('Done', CircleCheckBig, VIEW_TYPES.DONE)}
         <div className='w-full border-b-1 border-gray-300'></div>
         {lists.map(list => (
-          <TaskListFn
+          <TaskList
             key={list.id}
             list={list}
           />
