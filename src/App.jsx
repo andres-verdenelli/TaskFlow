92219bef09b6943c3bdbd085869749747e8df299
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-09 16:53:50 -0300

    Delete unnecesary div on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <div className='flex h-screen flex-col'>
       <Header />
-
       {/* el main content deberia creer el mismo, no desde un div de afuera */}
       <div className='flex grow'>
         <MainContent />
