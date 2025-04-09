167c3b75acf5e678d7e8d8168683e1af4a6b97d7
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-09 15:33:20 -0300

    Remove unnecesary div from Header.jsx

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -7,37 +7,33 @@ export default function Header({ setOpenSidebar }) {
   const { getCurrentViewName, currentView } = useTaskView()
 
   return (
-    <div>
-      <div>
-        <header>
-          <div className='grid grid-cols-3 border-b-1 border-b-gray-300 p-4'>
-            <div className='flex items-center'>
-              <button
-                className='p-1'
-                onClick={() => setOpenSidebar(true)}
-              >
-                <Menu />
-              </button>
-            </div>
-            <div className='flex items-center justify-center'>
-              <span className='text-xl'>{getCurrentViewName()}</span>
-            </div>
-            <div className='flex items-center justify-end'>
-              <button
-                className='p-1'
-                onClick={() => setOpenSidebar(true)}
-              >
-                {currentView.type === VIEW_TYPES.LIST && (
-                  <TaskListDialog
-                    mode='edit'
-                    listId={currentView.listId}
-                  />
-                )}
-              </button>
-            </div>
-          </div>
-        </header>
+    <header>
+      <div className='grid grid-cols-3 border-b-1 border-b-gray-300 p-4'>
+        <div className='flex items-center'>
+          <button
+            className='p-1'
+            onClick={() => setOpenSidebar(true)}
+          >
+            <Menu />
+          </button>
+        </div>
+        <div className='flex items-center justify-center'>
+          <span className='text-xl'>{getCurrentViewName()}</span>
+        </div>
+        <div className='flex items-center justify-end'>
+          <button
+            className='p-1'
+            onClick={() => setOpenSidebar(true)}
+          >
+            {currentView.type === VIEW_TYPES.LIST && (
+              <TaskListDialog
+                mode='edit'
+                listId={currentView.listId}
+              />
+            )}
+          </button>
+        </div>
       </div>
-    </div>
+    </header>
   )
 }
