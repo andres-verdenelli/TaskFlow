ba2c2c2c235e85ba027fae5151f89d5cceed08e1
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-09 21:27:10 -0300

    Implement TaskFrom on MainContent

--- src/layouts/MainContent.jsx
+++ src/layouts/MainContent.jsx
@@ -1,9 +1,13 @@
 import Task from '../components/Task'
 import { CreateTaskDialog } from '../components/CreateTaskDialog'
 import { useTaskView } from '../hooks/useTaskView'
+import { useState } from 'react'
+import { TaskForm } from '../components/TaskForm'
+import { Plus } from 'lucide-react'
 
 export default function MainContent() {
   const { visibleTasks } = useTaskView()
+  const [isTaskFormOpen, setTaskFormOpen] = useState(false)
 
   return (
     <div className='grow'>
@@ -15,7 +19,13 @@ export default function MainContent() {
           />
         ))}
       </ul>
-      <CreateTaskDialog />
+      <button
+        className='absolute right-4 bottom-4 rounded-full bg-black p-2 text-white'
+        onClick={() => setTaskFormOpen(true)}
+      >
+        <Plus />
+      </button>
+      {isTaskFormOpen && <TaskForm setTaskFormOpen={setTaskFormOpen} />}
     </div>
   )
 }
