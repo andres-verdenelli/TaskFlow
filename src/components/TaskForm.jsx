5f0a7e0a9cedc8744640b23f8e5b0f048d890e68
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-10 15:47:41 -0300

    Refactor TaskForm.jsx

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,58 +1,95 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useTasks } from '../hooks/useTasks'
 import { useTaskView } from '../hooks/useTaskView'
 import Button from './Button'
+import InputText from './InputText'
+import Label from './Label'
+import Select from './Select'
+import { useLists } from '../hooks/useLists'
 
 //implementar mandar formulario con enter
+//falta evitar enviar el formulario vacio
 export function TaskForm({ setTaskFormOpen }) {
   const { currentView } = useTaskView()
   const { createTask } = useTasks()
+  const { lists } = useLists()
 
   const [taskName, setTaskName] = useState('')
+  const [taskListId, setTaskListId] = useState(currentView.listId)
 
   const handleClose = () => setTaskFormOpen(false)
-  const handleSubmit = () => {
-    createTask(taskName, currentView.listId)
+  const handleSubmit = e => {
+    if (e && e.preventDefault) {
+      e.preventDefault()
+    }
+    createTask(taskName, taskListId)
     handleClose()
   }
+  const handleSelectChange = e => {
+    setTaskListId(e.target.value)
+  }
+
+  const handleKeyDown = e => {
+    if (e.key === 'Escape') {
+      handleClose()
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown)
+
+    const inputElement = document.getElementById('task-name-input')
+    inputElement && inputElement.focus()
+
+    return () => document.removeEventListener('keydown', handleKeyDown)
+  }, [])
 
   return (
     <>
-      <div className='fixed inset-0 z-50 bg-black opacity-15'></div>
+      <div
+        className='fixed inset-0 z-50 bg-black opacity-15'
+        onClick={handleClose}
+      ></div>
       <div className='fixed top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2'>
         <div className='rounded-xl bg-white p-6'>
-          <div className='mb-4'>
-            <h1 className='font-bold'>Create new task</h1>
-          </div>
-          <div className='mb-4'>
-            <label htmlFor=''>Task name</label>
-            <input
-              className='border-1'
-              type='text'
-              value={taskName}
-              onChange={e => setTaskName(e.target.value)}
-            />
-          </div>
-          <div className='mb-4'>
-            <label htmlFor=''>List Name</label>
-            <select
-              className='border-1'
-              name='et'
-              id=''
-            >
-              <option>test</option>
-              <option>test2</option>
-            </select>
-          </div>
-          <div className='flex justify-end'>
-            <Button
-              variant='outlined'
-              onClick={handleClose}
-            >
-              Cancel
-            </Button>
-            <Button onClick={handleSubmit}>Create</Button>
-          </div>
+          <form onSubmit={handleSubmit}>
+            <div className='mb-4'>
+              <h1 className='text-lg font-bold'>Create new task</h1>
+            </div>
+            <div className='mb-3'>
+              <Label>Task name</Label>
+              <InputText
+                id='task-name-input'
+                value={taskName}
+                onChange={e => setTaskName(e.target.value)}
+              />
+            </div>
+            <div className='mb-4'>
+              <Label>List</Label>
+              <Select
+                value={taskListId}
+                onChange={handleSelectChange}
+              >
+                {lists.map(list => (
+                  <option
+                    value={list.id}
+                    key={list.id}
+                  >
+                    {list.name}
+                  </option>
+                ))}
+              </Select>
+            </div>
+            <div className='flex justify-end gap-2'>
+              <Button
+                variant='outlined'
+                onClick={handleClose}
+              >
+                Cancel
+              </Button>
+              <Button type='submit'>Create</Button>
+            </div>
+          </form>
         </div>
       </div>
     </>
