f326434978da21641af9f6dab86ff7b4f3e71745
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-10 15:47:01 -0300

    Refactor styles and fix props on Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,11 @@ export default function Button({
   children,
   variant = 'primary',
   className = '',
-  ...props
+  props,
 }) {
   const baseClasses = 'rounded-lg border-1 px-3 py-1 font-light'
   const variantClasses = {
-    primary: 'bg-neutral-800 text-white',
+    primary: 'bg-neutral-800 text-white border-neutral-800',
     secondary: 'bg-zinc-100  border-zinc-200',
     destructive: 'bg-red-700 border-red-700 text-white',
     outline: 'bg-white border-zinc-300',
