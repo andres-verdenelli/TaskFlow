c07b4119a666e6d3d01599ea4e4914f0ceeb6a1d
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-11 13:19:24 -0300

    Remove unused code on TaskForm.jsx

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useTasks } from '../hooks/useTasks'
 import { useTaskView } from '../hooks/useTaskView'
 import Button from './Button'
@@ -6,43 +6,31 @@ import InputText from './InputText'
 import Label from './Label'
 import Select from './Select'
 import { useLists } from '../hooks/useLists'
+import { VIEW_TYPES } from '../constants/viewTypes'
 
-//implementar mandar formulario con enter
-//falta evitar enviar el formulario vacio
+//al apretar enter se manda el formulario pero no agrega la task
+//autofocus inputtext
+
 export function TaskForm({ setTaskFormOpen }) {
   const { currentView } = useTaskView()
   const { createTask } = useTasks()
   const { lists } = useLists()
 
   const [taskName, setTaskName] = useState('')
-  const [taskListId, setTaskListId] = useState(currentView.listId)
+  const [taskListId, setTaskListId] = useState(
+    currentView.type === VIEW_TYPES.LIST ? currentView.listId : lists[0].id,
+  )
 
   const handleClose = () => setTaskFormOpen(false)
-  const handleSubmit = e => {
-    if (e && e.preventDefault) {
-      e.preventDefault()
-    }
+
+  const handleSubmit = () => {
     createTask(taskName, taskListId)
     handleClose()
   }
+
   const handleSelectChange = e => {
     setTaskListId(e.target.value)
   }
-
-  const handleKeyDown = e => {
-    if (e.key === 'Escape') {
-      handleClose()
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown)
-
-    const inputElement = document.getElementById('task-name-input')
-    inputElement && inputElement.focus()
-
-    return () => document.removeEventListener('keydown', handleKeyDown)
-  }, [])
 
   return (
     <>
@@ -57,16 +45,20 @@ export function TaskForm({ setTaskFormOpen }) {
               <h1 className='text-lg font-bold'>Create new task</h1>
             </div>
             <div className='mb-3'>
-              <Label>Task name</Label>
+              <Label htmlFor='task-name-input'>Task name</Label>
               <InputText
+                name='task-name-input'
                 id='task-name-input'
                 value={taskName}
                 onChange={e => setTaskName(e.target.value)}
+                required
               />
             </div>
             <div className='mb-4'>
-              <Label>List</Label>
+              <Label htmlFor='list-select-input'>List</Label>
               <Select
+                id='list-select-input'
+                name='list-select-input'
                 value={taskListId}
                 onChange={handleSelectChange}
               >
