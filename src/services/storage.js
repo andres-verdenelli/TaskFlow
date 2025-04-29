f06ba5592df7d2b6f876d1572dc7f665a6b27ece
Andres Verdenelli <andresverdenelli@icloud.com>
2025-04-29 08:48:33 -0300

    Remove commented code on storage.js

--- src/services/storage.js
+++ src/services/storage.js
@@ -10,31 +10,3 @@ export const StorageService = {
     localStorage.removeItem(key)
   },
 }
-
-// export const StorageService = {
-//   get: key => {
-//     const item = localStorage.getItem(key)
-//     console.log(`Raw data from localStorage (${key}):`, item)
-
-//     if (!item || item === 'undefined' || item === 'null') {
-//       return null
-//     }
-
-//     try {
-//       return JSON.parse(item)
-//     } catch (error) {
-//       console.error(`❌ Error al parsear JSON de "${key}":`, error, item)
-//       return null // Evita que la app se rompa
-//     }
-//   },
-//   set: (key, value) => {
-//     if (value === undefined || value === null) {
-//       console.error(
-//         `❌ Intentaste guardar un valor inválido en "${key}"`,
-//         value
-//       )
-//       return
-//     }
-//     localStorage.setItem(key, JSON.stringify(value))
-//   },
-// }
